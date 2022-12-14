bc6b171e82f3182fbe7f8162e45b77af4816c544
Leito1982 <leonardo.otal@gmail.com>
2022-12-14 20:30:22 -0300

    configurando el render

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ const app = express();
 // requiero el modulo path para el ruteo de las paginas
 const path = require ('path');
 
+//********************************************** */
+// ESTO ES PARA CORRERLO EN FORMA LOCAL
 
 // declaro la variable port y le asigno el puerto de comunicion
-let PORT = 3000
+// let PORT = 3000
 
 // pongo en recepcion de request
-app.listen (PORT,() => {console.log ('el servidor esta funcionando '+ PORT)} );
+//app.listen (PORT,() => {console.log ('el servidor esta funcionando '+ PORT)} );
+
+//********************************************** */
+// ESTO ES PARA EL RENDER
+let port = process.env.PORT || 3000;
+app.listen (port,() => {console.log ('el servidor esta funcionando '+ port)} );
+
+
+
+
+
 
 // para tener acceso a los archivos de contenido NO OLVIDAR DE PONERLO
 app.use (express.static('public'));
